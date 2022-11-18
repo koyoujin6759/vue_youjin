f27c59169a3cd2dce620f04c9307dfbcdfa3c1dc
koyoujin6759 <rhdbwls853@naver.com>
2022-11-18 16:23:03 +0900

    20221118_header_gnb

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,37 +1,29 @@
 <template>
     <div>
         <header>
-            <ul class="gnb">
+            <!-- <ul class="gnb">
                 <li><router-link to="/">home</router-link></li>
                 <li><router-link to="/menu1">menu1</router-link></li>
                 <li><router-link to="/menu2">menu2</router-link></li>
                 <li><router-link to="/menu3">menu3</router-link></li>
-            </ul>
+            </ul> -->
+            <HeaderNav></HeaderNav>
         </header>
     </div>    
 </template>
 
 <script>
+import HeaderNav from '@/components/AppHeaderNav.vue'
+
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    components: {
+        HeaderNav
+    }
 }
 </script>
 
-<style scoped>
-header {
-    border-bottom:1px solid #ddd;
-    padding:0 20px;
-    height:80px;
-    line-height:80px;
-}
-ul,
-li {
-    padding:0;
-    margin:0; 
-    list-style: none;
-}
-.gnb li {
-    display: inline-block;
-    padding:0 30px;
-}
+<style lang="scss">
+// @import "../assets/scss/_variable.scss";
+
 </style>
---
+++ src/components/AppHeaderNav.vue
@@ -0,0 +1,125 @@
+<template>
+    <div>
+        <nav id="nav">
+            <ul class="gnb">
+                <li v-for="(menu,idx) in menus" v-bind:key="idx"> 
+                    <router-link :to="menu.url" v-if="!menu.children">{{menu.name}}</router-link>
+                    <div class="depth2"
+                        v-else
+                        v-on:mouseover="mouseover"
+                        v-on:mouseleave="mouseleave"
+                    >
+                        {{menu.name}}
+                        <ul class="dropdown" :class="{ isOpen }">
+                            <li v-for="(child,idx) in menu.children" v-bind:key="idx">
+                                <router-link to="/">{{child.name}}</router-link>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </nav>        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHeaderNav',
+
+    data() {
+        return {
+            isOpen:false,
+            menus: [
+                {
+                    url:'/',
+                    name:'Home'
+                },
+                {
+                    url:'/menu1',
+                    name:'Menu1'
+                },
+                {
+                    url:'/menu2',
+                    name:'Menu2',
+                    children: [
+                        {
+                            url:'/menu2-1',
+                            name:'Menu2-1'
+                        }
+                    ]
+                },
+                {
+                    url:'/menu3',
+                    name:'Menu3'
+                },
+            ]
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        mouseover: function() {
+            this.isOpen = true;
+        },
+        mouseleave: function() {
+            this.isOpen = false;
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+// @import "../assets/scss/_variable.scss";
+
+header {
+    border-bottom:1px solid #ddd;
+    padding:0 20px;
+    height:80px;
+    line-height:80px;
+
+    .gnb {
+        background:#eee;
+
+        & > li {
+            display: inline-block;
+            vertical-align: top;
+            padding:0 30px;
+            position:relative;
+            height:70px;
+            line-height:70px;
+            min-width:200px;
+            box-sizing:border-box;
+
+            a {
+                font-size:$primary-font-size;
+                color:$primary-color;
+                line-height:70px;
+            }
+
+            .depth2 {
+                position: relative;
+                display: block;
+                height: auto;
+                text-decoration: none;
+                cursor: pointer;
+
+                .dropdown {
+                    display:none;
+                    position:absolute;
+                    left:0;
+                    top:70px;
+                    width:100%;
+                    background:rgba(0,0,0,.3)
+                }
+            }            
+        }
+    }
+}
+
+.isOpen {
+    display:block !important;
+}
+</style>
